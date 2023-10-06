751cb6b82975bfb9cd41b272bdd46b00ba94afd4
Kacper Radota <72502652+KacperRadota@users.noreply.github.com>
2023-10-06 11:35:11 +0200

    Project structure

---
+++ preprocess.py
@@ -0,0 +1,1 @@
+# Script for converting and splitting the data - saves into data/processed
---
+++ train.py
@@ -0,0 +1,3 @@
+# Training loop
+# TODO import classes from models.py and dataset.py
+# TODO load the data from data/processed
